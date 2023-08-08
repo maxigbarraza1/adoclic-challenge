1f5abca80add150ba5ffa3d44fc9859d2abdbb07
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 18:10:08 -0300

    products module add forms modules

--- src/app/ui/pages/products/products.module.ts
+++ src/app/ui/pages/products/products.module.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../../shared/shared.module';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductsPageComponent } from './products-page/products-page.component';
 import { ProductsRoutingModule } from './products-routing.module';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
   declarations: [ProductsPageComponent, ProductDetailsComponent],
-  imports: [CommonModule, ProductsRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class ProductsModule {}
