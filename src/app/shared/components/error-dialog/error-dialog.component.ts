f075f57c8262433e66197fe651c6b0a87c0f4c4a
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 19:18:07 -0300

    error dialog receive data from parent

--- src/app/shared/components/error-dialog/error-dialog.component.ts
+++ src/app/shared/components/error-dialog/error-dialog.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-error-dialog',
   templateUrl: './error-dialog.component.html',
-  styleUrls: ['./error-dialog.component.scss']
+  styleUrls: ['./error-dialog.component.scss'],
 })
 export class ErrorDialogComponent {
+  @Input() errorMsg: string | null = null;
 
+  constructor(private _modalService: NgbModal) {}
+
+  closeDialog(): void {
+    this._modalService.dismissAll();
+  }
 }
