7f95843a9fdfd91c024fb6a02640a16e20c1e625
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 16:32:10 -0300

    auth module imports shared module

--- src/app/ui/auth/auth.module.ts
+++ src/app/ui/auth/auth.module.ts
@@ -1,17 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 
-
 @NgModule({
-  declarations: [
-    LoginPageComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [LoginPageComponent],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule, SharedModule],
 })
-export class AuthModule { }
+export class AuthModule {}
