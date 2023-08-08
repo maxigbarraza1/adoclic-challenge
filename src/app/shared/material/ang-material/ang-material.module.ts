291c63099b4a73981da5e085ee3941bc24be2d3b
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 16:31:57 -0300

    angular material module

---
+++ src/app/shared/material/ang-material/ang-material.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const modules: any[] = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatDividerModule,
+];
+
+@NgModule({
+  exports: [...modules],
+})
+export class AngMaterialModule {}
