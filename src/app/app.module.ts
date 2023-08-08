ec5238e3bd2c53c788d887df136babbca8a1130f
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 18:08:19 -0300

    update providers app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GlobalConstants } from './core/utils/global-constants';
 import { AUTH_API_PROVIDER } from './domain/auth/infrastructure/providers/auth-api.provider';
 import { PRODUCT_API_PROVIDER } from './domain/products/infrastructure/providers/products-api.provider';
 
@@ -18,7 +19,7 @@ import { PRODUCT_API_PROVIDER } from './domain/products/infrastructure/providers
     NgbModule,
     HttpClientModule,
   ],
-  providers: [PRODUCT_API_PROVIDER, AUTH_API_PROVIDER],
+  providers: [PRODUCT_API_PROVIDER, AUTH_API_PROVIDER, GlobalConstants],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
