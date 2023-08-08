1be189c38811927d018c4874ee8e8cc7a0d71485
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 16:33:44 -0300

    products page set structure

--- src/app/ui/pages/products/products-page/products-page.component.ts
+++ src/app/ui/pages/products/products-page/products-page.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ProductDetailsComponent } from '../components/product-details/product-details.component';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
-  styleUrls: ['./products-page.component.scss']
+  styleUrls: ['./products-page.component.scss'],
 })
 export class ProductsPageComponent {
+  displayedColumns: string[] = ['id', 'title', 'category', 'price', 'actions'];
+  products: any[] = [
+    {
+      id: 1,
+      title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+      price: 109.95,
+      description:
+        'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+      category: "men's clothing",
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+      rating: {
+        rate: 3.9,
+        count: 120,
+      },
+    },
+  ];
 
+  constructor(private _modalService: NgbModal) {}
+
+  openProductDetails(): void {
+    this._modalService.open(ProductDetailsComponent);
+  }
 }
