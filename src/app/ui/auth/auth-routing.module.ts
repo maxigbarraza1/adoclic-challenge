bf7abbf3485fae40405a4b94cfe40685689dd4bb
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 13:20:07 -0300

    set wildcards and guard to routes

--- src/app/ui/auth/auth-routing.module.ts
+++ src/app/ui/auth/auth-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [{ path: '', component: LoginPageComponent }];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: '**', redirectTo: 'login' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
