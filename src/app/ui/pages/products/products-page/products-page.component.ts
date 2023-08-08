adda330b90531d889c5ad164baffb4e606713d26
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 18:11:08 -0300

    set products page logic & structure html

--- src/app/ui/pages/products/products-page/products-page.component.ts
+++ src/app/ui/pages/products/products-page/products-page.component.ts
@@ -1,5 +1,19 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, Subscription, map, startWith } from 'rxjs';
+import { AuthService } from '../../../../domain/auth/application/auth.service';
+import { ProductsService } from '../../../../domain/products/application/products.service';
+import { IDomainRequestProduct } from '../../../../domain/products/domain/product.model';
 import { ProductDetailsComponent } from '../components/product-details/product-details.component';
 
 @Component({
@@ -7,27 +21,83 @@ import { ProductDetailsComponent } from '../components/product-details/product-d
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.scss'],
 })
-export class ProductsPageComponent {
-  displayedColumns: string[] = ['id', 'title', 'category', 'price', 'actions'];
-  products: any[] = [
-    {
-      id: 1,
-      title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-      price: 109.95,
-      description:
-        'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-      category: "men's clothing",
-      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-      rating: {
-        rate: 3.9,
-        count: 120,
-      },
-    },
-  ];
+export class ProductsPageComponent implements OnInit, AfterViewInit, OnDestroy {
+  products: MatTableDataSource<IDomainRequestProduct> =
+    new MatTableDataSource<IDomainRequestProduct>();
 
-  constructor(private _modalService: NgbModal) {}
+  filteredOptions: Observable<string[]> = new Observable<string[]>();
+  filterFormControl = new FormControl('');
+  options: string[] = [];
+
+  displayedColumns: string[] = ['id', 'title', 'category', 'price', 'actions'];
+  pageSizeOptions = [5, 10, 20];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  private subscriptions: Subscription[] = [];
+
+  constructor(
+    private _modalService: NgbModal,
+    private _productsService: ProductsService,
+    private _authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    const productServiceSub1 = this._productsService
+      .getProducts()
+      .subscribe((products) => {
+        this.products = new MatTableDataSource(products);
+        this.products.paginator = this.paginator;
+        this.products.sort = this.sort;
+      });
+    const productServiceSub2 = this._productsService
+      .getProductCategories()
+      .subscribe((categories) => {
+        this.options = categories;
+      });
+    this.filteredOptions = this.filterFormControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filterAutocomplete(value || ''))
+    );
+    this.subscriptions.push(productServiceSub1, productServiceSub2);
+  }
+
+  ngAfterViewInit(): void {
+    this.products.paginator = this.paginator;
+    this.products.sort = this.sort;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
 
   openProductDetails(): void {
     this._modalService.open(ProductDetailsComponent);
+  }
+
+  logout(): void {
+    this._authService.logout();
+  }
+
+  private _applyFilter(value: string) {
+    const filterValue = value;
+    this.products.filter = filterValue.trim().toLowerCase();
+
+    if (this.products.paginator) {
+      this.products.paginator.firstPage();
+    }
+  }
+
+  private _filterAutocomplete(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    if (this.options.includes(filterValue)) {
+      this._applyFilter(filterValue);
+    } else {
+      this._applyFilter('');
+    }
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
   }
 }
