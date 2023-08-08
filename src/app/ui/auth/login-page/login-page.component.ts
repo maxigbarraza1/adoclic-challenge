b8063c87756031b38bf1fcef1c8568742322e8bb
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 16:32:24 -0300

    login page set structure & styles

--- src/app/ui/auth/login-page/login-page.component.ts
+++ src/app/ui/auth/login-page/login-page.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../domain/auth/application/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  loginForm: FormGroup = this._fb.group({});
+  isLoading: boolean = false;
 
+  constructor(
+    private _fb: FormBuilder,
+    private _authService: AuthService,
+    private _router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this._formBuilder();
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid || this.isLoading) {
+      this.loginForm.markAllAsTouched();
+    }
+    this.isLoading = true;
+    const { email, password } = this.loginForm.value;
+    this._authService.login(email, password).subscribe({
+      next: (resp) => {
+        console.log(resp);
+        if (resp.success) {
+          this._router.navigate(['/']);
+        } else {
+          console.log('ERROR: ', resp.error);
+        }
+      },
+      error: () => {},
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+  }
+
+  private _formBuilder(): void {
+    this.loginForm = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
 }
