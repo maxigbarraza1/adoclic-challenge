69a43237a5c4d702f7ee082e8b003cb529d9f98b
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 18:42:29 -0300

    login page set styles

--- src/app/ui/auth/login-page/login-page.component.ts
+++ src/app/ui/auth/login-page/login-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from '../../../domain/auth/application/auth.service';
+import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-login-page',
@@ -15,7 +17,8 @@ export class LoginPageComponent implements OnInit {
   constructor(
     private _fb: FormBuilder,
     private _authService: AuthService,
-    private _router: Router
+    private _router: Router,
+    private _modalService: NgbModal
   ) {}
 
   ngOnInit(): void {
@@ -25,22 +28,26 @@ export class LoginPageComponent implements OnInit {
   onSubmit(): void {
     if (this.loginForm.invalid || this.isLoading) {
       this.loginForm.markAllAsTouched();
+      return;
     }
     this.isLoading = true;
-    const { email, password } = this.loginForm.value;
-    this._authService.login(email, password).subscribe({
-      next: (resp) => {
-        if (resp.success) {
-          this._router.navigate(['/']);
-        } else {
-          console.log('ERROR: ', resp.error);
-        }
-      },
-      error: () => {},
-      complete: () => {
-        this.isLoading = false;
-      },
-    });
+    setTimeout(() => {
+      const { email, password } = this.loginForm.value;
+      this._authService.login(email, password).subscribe({
+        next: (resp) => {
+          if (resp.success) {
+            this._router.navigate(['/']);
+          } else {
+            this._openErrorDialog();
+            console.log('ERROR: ', resp.error);
+          }
+        },
+        error: () => {},
+        complete: () => {
+          this.isLoading = false;
+        },
+      });
+    }, 1500);
   }
 
   private _formBuilder(): void {
@@ -48,5 +55,9 @@ export class LoginPageComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+  }
+
+  private _openErrorDialog(): void {
+    this._modalService.open(ErrorDialogComponent);
   }
 }
