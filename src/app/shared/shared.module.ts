dd77f6f782a75b2171321a34f667d9a9bd95081e
Maxi Barraza <barrazagabrielm@gmail.com>
2023-08-08 18:41:59 -0300

    new component error dialog

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AngMaterialModule } from './material/ang-material/ang-material.module';
 import { ErrorMessagePipe } from './pipes/error-message.pipe';
+import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 
 @NgModule({
-  declarations: [ErrorMessagePipe],
+  declarations: [ErrorMessagePipe, ErrorDialogComponent],
   imports: [CommonModule, AngMaterialModule],
   exports: [AngMaterialModule, ErrorMessagePipe],
 })
